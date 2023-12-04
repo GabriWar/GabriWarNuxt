e0e9820afd7292b74ea0f009bb7f47cf8b05b098
Gabriel Duarte Guerra <gabrielduarteg02@gmail.com>
2023-12-04 15:45:52 -0300

    separated in categories

--- components/portfolio.vue
+++ components/portfolio.vue
@@ -20,83 +20,92 @@
     <div class="txtcontainer">
       <div class="resumeEN">
         <div>
-          <a class="txtanim link-color">Resume</a>
+          <div class="resume">
+            <a class="txtanim link-color">Resume</a>
 
-          <p>
-            Hello, I'm Gabriel, a passionate Full Stack Developer currently
-            studying Computer Science. With a deep-rooted love for Linux, I
-            consider myself a power user,I love libre and open-source software.
-            My curiosity extends beyond coding, I'm also proficient in image
-            manipulation, And my love for debugging apps and modding Android
-            phones adds to my technical skills.
-          </p>
-          <a class="txtanim link-color-web">WebDev</a>
-          <p>
-            Proficient in both front-end and back-end technologies, experienced
-            with HTML, CSS, JavaScript (<a class="txtanim link-color-react"
-              >React/Next</a
-            >, <a class="txtanim link-color-vue">Vue/Nuxt</a>,
-            <a class="txtanim link-color-rust">Svelte/SvelteKit</a>), and
-            server-side scripting languages (<a
-              class="txtanim link-color-projects"
-              >Node.js</a
-            >, <a class="txtanim link-color-cpp">C++</a>,
-            <a class="txtanim link-color-rust">Rust</a>,
-            <a class="txtanim link-color-py">Python</a>).
-          </p>
-          <a class="txtanim txtanim link-color-linux">Linux </a>
-          <p>
-            Proficient with the
-            <a class="txtanim link-color-linux">Linux</a> command line
-            operation, comfortable with
-            <a class="txtanim link-color">Shell Scripting</a> and automation,
-            adept at optimizing system performance and troubleshooting.
-          </p>
-
-          <a class="txtanim txtanim link-color-edu">Education</a>
-          <p>
-            Currently pursuing a degree in Computer Science, I am dedicated to
-            expanding my knowledge and staying at the forefront of technology. I
-            also speak Portuguese (native), English (fluent), German (learning).
-          </p>
-          <a class="txtanim txtanim link-color-projects">Projects</a>&nbsp
-          <p>
-            <a
-              class="txtanim link-color-projects"
-              href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=&sort="
-              >GitHub</a
-            >&nbsp
-            <a
-              class="txtanim txtanim link-color-js"
-              href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=javascript&sort="
-              >JavaScript</a
-            >&nbsp
-            <a
-              class="txtanim txtanim link-color-py"
-              href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=python&sort="
-              >Python</a
-            >&nbsp
-            <a
-              class="txtanim txtanim link-color-cpp"
-              href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=c%2B%2B&sort="
-              >C++</a
-            >&nbsp
-            <a
-              class="txtanim txtanim link-color-rust"
-              href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=rust&sort="
-              >Rust</a
-            >&nbsp
-            <a
-              class="txtanim txtanim link-color-react"
-              href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=javascript&sort="
-              >React</a
-            >&nbsp
-            <a
-              class="txtanim txtanim link-color-vue"
-              href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=vue&sort="
-              >Vue</a
-            >&nbsp
-          </p>
+            <p>
+              Hello, I'm Gabriel, a passionate Full Stack Developer currently
+              studying Computer Science. With a deep-rooted love for Linux, I
+              consider myself a power user,I love libre and open-source
+              software. My curiosity extends beyond coding, I'm also proficient
+              in image manipulation, And my love for debugging apps and modding
+              Android phones adds to my technical skills.
+            </p>
+          </div>
+          <div class="WebDev">
+            <a class="txtanim link-color-web">WebDev</a>
+            <p>
+              Proficient in both front-end and back-end technologies,
+              experienced with HTML, CSS, JavaScript (<a
+                class="txtanim link-color-react"
+                >React/Next</a
+              >, <a class="txtanim link-color-vue">Vue/Nuxt</a>,
+              <a class="txtanim link-color-rust">Svelte/SvelteKit</a>), and
+              server-side scripting languages (<a
+                class="txtanim link-color-projects"
+                >Node.js</a
+              >, <a class="txtanim link-color-cpp">C++</a>,
+              <a class="txtanim link-color-rust">Rust</a>,
+              <a class="txtanim link-color-py">Python</a>).
+            </p>
+            <a class="txtanim txtanim link-color-linux">Linux </a>
+            <p>
+              Proficient with the
+              <a class="txtanim link-color-linux">Linux</a> command line
+              operation, comfortable with
+              <a class="txtanim link-color">Shell Scripting</a> and automation,
+              adept at optimizing system performance and troubleshooting.
+            </p>
+          </div>
+          <div class="education">
+            <a class="txtanim txtanim link-color-edu">Education</a>
+            <p>
+              Currently pursuing a degree in Computer Science, I am dedicated to
+              expanding my knowledge and staying at the forefront of technology.
+              I also speak Portuguese (native), English (fluent), German
+              (learning).
+            </p>
+          </div>
+          <div class="projects">
+            <a class="txtanim txtanim link-color-projects">Projects</a>&nbsp
+            <p>
+              <a
+                class="txtanim link-color-projects"
+                href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=&sort="
+                >GitHub</a
+              >&nbsp
+              <a
+                class="txtanim txtanim link-color-js"
+                href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=javascript&sort="
+                >JavaScript</a
+              >&nbsp
+              <a
+                class="txtanim txtanim link-color-py"
+                href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=python&sort="
+                >Python</a
+              >&nbsp
+              <a
+                class="txtanim txtanim link-color-cpp"
+                href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=c%2B%2B&sort="
+                >C++</a
+              >&nbsp
+              <a
+                class="txtanim txtanim link-color-rust"
+                href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=rust&sort="
+                >Rust</a
+              >&nbsp
+              <a
+                class="txtanim txtanim link-color-react"
+                href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=javascript&sort="
+                >React</a
+              >&nbsp
+              <a
+                class="txtanim txtanim link-color-vue"
+                href="https://github.com/GabriWar?tab=repositories&q=&type=source&language=vue&sort="
+                >Vue</a
+              >&nbsp
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -106,15 +115,17 @@
 <style scoped>
 @import url(assets/css/hoveranim.css);
 
-.all{
+.all {
   height: 90vh;
   overflow: hidden;
 }
+
 .resumeEN {
   z-index: 1;
   font-size: clamp(1.2rem, 1.5vw, 1.4rem);
   margin-top: 10px;
 }
+
 .txtcontainer {
   display: grid;
   grid-template-columns: 1fr;
@@ -133,11 +144,13 @@
   font-size: clamp(50px, 8vw, 75px);
   cursor: pointer;
 }
+
 .bgcontainer {
   z-index: -1;
   margin-top: -4rem;
   overflow: visible;
 }
+
 .txtdiv {
   position: absolute;
   width: 100%;
@@ -151,14 +164,19 @@
     black 10%,
     transparent 50%
   );
-    top: 50%;
+  top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   font-size: 2rem;
 }
+
 .randtxt {
   position: absolute;
-  mask-image: radial-gradient(circle at var(--x) var(--y), black 1%, transparent 50%);
+  mask-image: radial-gradient(
+    circle at var(--x) var(--y),
+    black 1%,
+    transparent 50%
+  );
   -webkit-mask-image: radial-gradient(
     circle at var(--x) var(--y),
     black 1%,
@@ -168,27 +186,31 @@
   margin: 0%;
   padding: 0%;
 }
+
 @media screen and (max-width: 800px) {
   .txt {
     mask-image: none;
     -webkit-mask-image: none;
   }
+
   .txtdiv {
     mask-image: none;
     -webkit-mask-image: none;
     height: 100vh;
   }
+
   .txtcontainer {
     width: 100%;
   }
+
   .randtxt {
     color: rgb(26, 26, 26);
     height: 100vh;
   }
+
   .name {
     display: none;
   }
-
 }
 </style>
 
