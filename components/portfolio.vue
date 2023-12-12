7c18d9175da9505802a246335704eb89ed85fcfc
Gabriel Duarte Guerra <gabrielduarteg02@gmail.com>
2023-12-12 09:55:35 -0300

    boy we are soo back
    we ball
    
    nothing "changed" per say, but im trying to implement a new page, kinda cool

--- components/portfolio.vue
+++ components/portfolio.vue
@@ -11,7 +11,11 @@
 
       <div class="bttn-translate" @click="translatetext">BR/EN</div>
 
-      <div class="name" id="animate" @mouseover="animaText('animate', 'Gabriel', 'Guerra')">
+      <div
+        class="name"
+        id="animate"
+        @mouseover="animaText('animate', 'Gabriel', 'Guerra')"
+      >
         GabriWar
       </div>
     </div>
@@ -19,9 +23,17 @@
     <div class="txtcontainer">
       <div class="resumeEN">
         <div class="resume" data-visible="1">
-          <a class="txtanim link-color" data-translate="Resumo" @click="handleclick('resumetxt')">Resume</a>
-          <p class="resumetxt" data-visible="0"
-            data-translate="Olá, eu sou o Gabriel, um entusiasta Desenvolvedor Full Stack que atualmente está estudando Ciência da Computação. Com um amor profundo pelo Linux, me considero um usuário avançado. Eu adoro software livre e de código aberto. Minha curiosidade vai além da programação; também sou habilidoso em manipulação de imagens. Além disso, meu amor por debugar aplicativos e fazer modificações em telefones Android contribui para minhas habilidades técnicas.">
+          <a
+            class="txtanim link-color"
+            data-translate="Resumo"
+            @click="handleclick('resumetxt')"
+            >Resume</a
+          >
+          <p
+            class="resumetxt"
+            data-visible="0"
+            data-translate="Olá, eu sou o Gabriel, um entusiasta Desenvolvedor Full Stack que atualmente está estudando Ciência da Computação. Com um amor profundo pelo Linux, me considero um usuário avançado. Eu adoro software livre e de código aberto. Minha curiosidade vai além da programação; também sou habilidoso em manipulação de imagens. Além disso, meu amor por debugar aplicativos e fazer modificações em telefones Android contribui para minhas habilidades técnicas."
+          >
             Hello, I'm Gabriel, a passionate Full Stack Developer currently
             studying Computer Science. With a deep-rooted love for Linux, I
             consider myself a power user,I love libre and open-source software.
@@ -31,52 +43,112 @@
           </p>
         </div>
         <div class="WebDev" data-visible="1">
-          <a class="txtanim link-color-web" data-translate="WebDev" @click="handleclick('resumeWebDev')">WebDev</a>
-          <p class="resumeWebDev" data-visible="0"
-            data-translate="Proficiente tanto nas tecnologias de front-end quanto de back-end, com experiência em HTML, CSS e JavaScript (React/Next, Vue/Nuxt, Svelte/SvelteKit), e linguagens server-side/BackEnd (Node.js, C++, Rust, Python).">
+          <a
+            class="txtanim link-color-web"
+            data-translate="WebDev"
+            @click="handleclick('resumeWebDev')"
+            >WebDev</a
+          >
+          <p
+            class="resumeWebDev"
+            data-visible="0"
+            data-translate="Proficiente tanto nas tecnologias de front-end quanto de back-end, com experiência em HTML, CSS e JavaScript (React/Next, Vue/Nuxt, Svelte/SvelteKit), e linguagens server-side/BackEnd (Node.js, C++, Rust, Python)."
+          >
             Proficient in both front-end and back-end technologies, experienced
-            with HTML, CSS, JavaScript (<a class="txtanim link-color-react" to="/links/2">React/Next</a>,
-            <NuxtLink class="txtanim link-color-vue" to="/links/3">Vue/Nuxt</NuxtLink>,
-            <NuxtLink class="txtanim link-color-rust" to="/links/2">Svelte/SvelteKit</NuxtLink>),
-            and server-side scripting languages (<a class="txtanim link-color-projects" to="/links/2">Node.js</a>,
-            <NuxtLink class="txtanim link-color-cpp" to="/links/4">C++</NuxtLink>,
-            <NuxtLink class="txtanim link-color-rust" to="/links/5">Rust</NuxtLink>,
-            <NuxtLink class="txtanim link-color-py" to="/links/6">Python</NuxtLink>).
+            with HTML, CSS, JavaScript (<a
+              class="txtanim link-color-react"
+              to="/links/2"
+              >React/Next</a
+            >,
+            <NuxtLink class="txtanim link-color-vue" to="/links/3"
+              >Vue/Nuxt</NuxtLink
+            >,
+            <NuxtLink class="txtanim link-color-rust" to="/links/2"
+              >Svelte/SvelteKit</NuxtLink
+            >), and server-side scripting languages (<a
+              class="txtanim link-color-projects"
+              to="/links/2"
+              >Node.js</a
+            >,
+            <NuxtLink class="txtanim link-color-cpp" to="/links/4">C++</NuxtLink
+            >,
+            <NuxtLink class="txtanim link-color-rust" to="/links/5"
+              >Rust</NuxtLink
+            >,
+            <NuxtLink class="txtanim link-color-py" to="/links/6"
+              >Python</NuxtLink
+            >).
           </p>
         </div>
         <div class="linux" data-visible="1">
-          <a class="txtanim link-color-linux" data-translate="Linux" @click="handleclick('resumelinux')">Linux</a>
-          <p class="resumelinux" data-visible="0"
-            data-translate="Proficiente com o terminal linux, confortavel com a operação na linha de comando, Shell Scripting, e automação, habilidoso em optimização de performance e troubleshooting">
+          <a
+            class="txtanim link-color-linux"
+            data-translate="Linux"
+            @click="handleclick('resumelinux')"
+            >Linux</a
+          >
+          <p
+            class="resumelinux"
+            data-visible="0"
+            data-translate="Proficiente com o terminal linux, confortavel com a operação na linha de comando, Shell Scripting, e automação, habilidoso em optimização de performance e troubleshooting"
+          >
             Proficient with the
-            <NuxtLink class="txtanim link-color-linux" to="/links/7">Linux</NuxtLink>
+            <NuxtLink class="txtanim link-color-linux" to="/links/7"
+              >Linux</NuxtLink
+            >
             command line operation, comfortable with
             <a class="txtanim link-color">Shell Scripting</a> and automation,
             adept at optimizing system performance and troubleshooting.
           </p>
         </div>
         <div class="edu" data-visible="1">
-          <a class="txtanim link-color-edu" data-translate="Educação" @click="handleclick('resumeedu')">Education</a>
-          <p class="resumeedu" data-visible="0"
-            data-translate="Atualmente cursando uma graduação em Ciência da Computação, estou dedicado(a) a ampliar meu conhecimento e permanecer na vanguarda da tecnologia. Também falo português (nativo), inglês (fluente) e estou aprendendo alemão.">
+          <a
+            class="txtanim link-color-edu"
+            data-translate="Educação"
+            @click="handleclick('resumeedu')"
+            >Education</a
+          >
+          <p
+            class="resumeedu"
+            data-visible="0"
+            data-translate="Atualmente cursando uma graduação em Ciência da Computação, estou dedicado(a) a ampliar meu conhecimento e permanecer na vanguarda da tecnologia. Também falo português (nativo), inglês (fluente) e estou aprendendo alemão."
+          >
             Currently pursuing a degree in Computer Science, I am dedicated to
             expanding my knowledge and staying at the forefront of technology. I
             also speak Portuguese (native), English (fluent), German (learning).
           </p>
         </div>
         <div class="proj" data-visible="1">
-          <a class="txtanim link-color-projects projtxt" data-translate="Projetos"
-            @click="handleclick('resumeproj')">Projects</a>
+          <a
+            class="txtanim link-color-projects projtxt"
+            data-translate="Projetos"
+            @click="handleclick('resumeproj')"
+            >Projects</a
+          >
           <!-- had to get an special class for this one -->
           &nbsp
           <p class="resumeproj" data-visible="0">
-            <NuxtLink class="txtanim link-color-projects" to="/links/1">GitHub</NuxtLink>&nbsp
-            <NuxtLink class="txtanim txta\nim link-color-js" to="/links/2">JavaScript</NuxtLink>&nbsp
-            <NuxtLink class="txtanim txtanim link-color-py" to="/links/6">Python</NuxtLink>&nbsp
-            <NuxtLink class="txtanim txtanim link-color-cpp" to="/links/4">C++</NuxtLink>&nbsp
-            <NuxtLink class="txtanim txtanim link-color-rust" to="/links/5">Rust</NuxtLink>&nbsp
-            <NuxtLink class="txtanim txtanim link-color-react" to="/links/2">React</NuxtLink>&nbsp
-            <NuxtLink class="txtanim txtanim link-color-vue" to="/links/3">Vue</NuxtLink>&nbsp
+            <NuxtLink class="txtanim link-color-projects" to="/links/1"
+              >GitHub</NuxtLink
+            >&nbsp
+            <NuxtLink class="txtanim txta\nim link-color-js" to="/links/2"
+              >JavaScript</NuxtLink
+            >&nbsp
+            <NuxtLink class="txtanim txtanim link-color-py" to="/links/6"
+              >Python</NuxtLink
+            >&nbsp
+            <NuxtLink class="txtanim txtanim link-color-cpp" to="/links/4"
+              >C++</NuxtLink
+            >&nbsp
+            <NuxtLink class="txtanim txtanim link-color-rust" to="/links/5"
+              >Rust</NuxtLink
+            >&nbsp
+            <NuxtLink class="txtanim txtanim link-color-react" to="/links/2"
+              >React</NuxtLink
+            >&nbsp
+            <NuxtLink class="txtanim txtanim link-color-vue" to="/links/3"
+              >Vue</NuxtLink
+            >&nbsp
           </p>
         </div>
       </div>
---
+++ components/test.vue
@@ -0,0 +1,152 @@
+<template>
+  <section id="demos" class="invisible">
+    <video id="webcam" autoplay playsinline></video>
+    <canvas
+      class="output_canvas"
+      id="output_canvas"
+      width="1280"
+      height="720"
+      style="position: absolute; left: 0px; top: 0px"
+    ></canvas>
+    <p id="gesture_output" class="output"></p>
+  </section>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeMount } from 'vue';
+import {
+  FilesetResolver,
+  GestureRecognizer,
+  DrawingUtils,
+} from '@mediapipe/tasks-vision';
+
+let gestureRecognizer = ref(null);
+let webcamRunning = false;
+const videoHeight = '360px';
+const videoWidth = '480px';
+const createGestureRecognizer = async () => {
+  const vision = await FilesetResolver.forVisionTasks(
+    'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm'
+  );
+
+  gestureRecognizer.value = await GestureRecognizer.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath:
+        'https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task',
+      delegate: 'GPU',
+    },
+  });
+  console.log('createGestureRecognizer', gestureRecognizer.value);
+};
+onBeforeMount(async () => {
+  video.value = document.getElementById('webcam');
+  canvasElement.value = document.getElementById('output_canvas');
+  demosSection.value = document.getElementById('demos');
+  gestureOutput.value = document.getElementById('gesture_output');
+  await createGestureRecognizer();
+  async function predictWebcam() {
+    console.log('predictWebcam');
+    const webcamElement = video.value;
+    let nowInMs = Date.now();
+    if (video.value.currentTime !== lastVideoTime) {
+      lastVideoTime = video.value.currentTime;
+      console.log(
+        'predictWebcam',
+        lastVideoTime,
+        results.value,
+        gestureRecognizer.value
+      );
+      results.value = gestureRecognizer.value.detectForVideo(
+        video.value,
+        nowInMs
+      );
+    }
+
+    if (webcamElement) {
+      canvasElement.value.getContext('2d').save();
+      canvasElement.value
+        .getContext('2d')
+        .clearRect(0, 0, canvasElement.value.width, canvasElement.value.height);
+      const drawingUtils = new DrawingUtils(
+        canvasElement.value.getContext('2d')
+      );
+      canvasElement.value.style.height = videoHeight;
+      webcamElement.style.height = videoHeight;
+      canvasElement.value.style.width = videoWidth;
+      webcamElement.style.width = videoWidth;
+      if (results.value.landmarks) {
+        for (const landmarks of results.value.landmarks) {
+          drawingUtils.drawConnectors(
+            landmarks,
+            GestureRecognizer.HAND_CONNECTIONS,
+            {
+              color: '#00FF00',
+              lineWidth: 5,
+            }
+          );
+          drawingUtils.drawLandmarks(landmarks, {
+            color: '#FF0000',
+            lineWidth: 2,
+          });
+        }
+      }
+      canvasElement.value.getContext('2d').restore();
+      if (results.value.gestures.length > 0) {
+        gestureOutput.value.style.width = videoWidth;
+        const categoryName = results.value.gestures[0][0].categoryName;
+        const categoryScore = parseFloat(
+          results.value.gestures[0][0].score * 100
+        ).toFixed(2);
+        const handedness = results.value.handednesses[0][0].displayName;
+        gestureOutput.value.innerText = `GestureRecognizer: ${categoryName}\n Confidence: ${categoryScore} %\n Handedness: ${handedness}`;
+      } else {
+        gestureOutput.value.style.display = 'none';
+      }
+      if (webcamRunning === true) {
+        window.requestAnimationFrame(predictWebcam);
+      }
+    }
+  }
+  predictWebcamRef.value = predictWebcam;
+});
+onMounted(async () => {
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+      video.value.srcObject = stream;
+      video.value.addEventListener('loadeddata', predictWebcamRef.value);
+    });
+  } else {
+    console.error('getUserMedia is not supported');
+  }
+  console.log('onMounted');
+  console.log('onMounted', gestureRecognizer.value);
+});
+
+console.log(
+  'created?',
+  createGestureRecognizer(),
+  createGestureRecognizer.value
+);
+const demosSection = ref(null);
+const video = ref(null);
+const canvasElement = ref(null);
+const gestureOutput = ref(null);
+const predictWebcamRef = ref(null);
+const results = ref(null);
+
+const constraints = {
+  video: true,
+};
+
+let lastVideoTime = -1;
+</script>
+<style scoped>
+webcam {
+  position: absolute;
+  margin: auto;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+</style>
